8e1b06b7b3a222569968a9dc9fbeee26f60beaa5
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2023-01-25 10:46:28 -0800

    Initial Commit

---
+++ ll-mux-test/ll_mux_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ll_mux_testApp.swift
+//  ll-mux-test
+//
+//  Created by Dylan Jhaveri on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ll_mux_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
